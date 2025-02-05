eec08fd9a77f499ea3742a8e800b1e4903cc16da
Nicolas Diaz <52494185+Nicodc96@users.noreply.github.com>
2025-02-05 00:45:02 -0300

    Update LinkedList.h

--- Laboratorio I/Trabajo Practico N.4/LinkedList.h
+++ Laboratorio I/Trabajo Practico N.4/LinkedList.h
@@ -1,21 +1,3 @@
-/*
-    utest example : Unit test examples.
-    Copyright (C) <2018>  <Mauricio Davila>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #ifndef __LINKEDLIST
 #define __LINKEDLIST
 struct Node
@@ -31,7 +13,6 @@ struct LinkedList
 }typedef LinkedList;
 #endif
 
-// Funciones BASE de Linkedlist
 LinkedList* ll_newLinkedList(void);
 int ll_len(LinkedList* this);
 Node* test_getNode(LinkedList* this, int nodeIndex);
@@ -45,12 +26,11 @@ int ll_deleteLinkedList(LinkedList* this);
 int ll_indexOf(LinkedList* this, void* pElement);
 int ll_isEmpty(LinkedList* this);
 int ll_push(LinkedList* this, int index, void* pElement);
-void* ll_pop(LinkedList* this,int index);                           //elimina un elemento (los enlaces a ese elemento) y devuelve un puntero a ese elemento
+void* ll_pop(LinkedList* this,int index);
 int ll_contains(LinkedList* this, void* pElement);
 int ll_containsAll(LinkedList* this,LinkedList* this2);
 LinkedList* ll_subList(LinkedList* this,int from,int to);
 LinkedList* ll_clone(LinkedList* this);
 int ll_sort(LinkedList* this, int (*pFunc)(void* ,void*), int order);
 
-// Funcion desarrollada en clase
 LinkedList* ll_filter(LinkedList* this, int (*pFunc)(void*));
